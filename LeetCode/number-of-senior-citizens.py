5efe15ac418c38797e82d5d9375fc1606a7ea0a9
Ransford Genesis <83247407+RansfordGenesis@users.noreply.github.com>
2023-05-13 16:33:34 +0000

    Create number-of-senior-citizens.py

---
+++ LeetCode/number-of-senior-citizens.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count = 0
+        count = 0
+        for each in details:
+            if int(each[11:13]) > 60:
+                count += 1
+        return count
