6ec2b11420e605211c4fd356d7ba3a9031b215ec
Ransford Genesis <83247407+RansfordGenesis@users.noreply.github.com>
2023-05-24 00:01:27 +0000

    Delete LeetCode/smallest-index-with-equal-value. directory

--- LeetCode/smallest-index-with-equal-value./smallest-index-with-equal-value.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def smallestEqual(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
-            if i % 10 == nums[i]:
-                return i
-                break
-        else:
-            return -1
