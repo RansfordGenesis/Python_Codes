5bcc19d05888924e3bb0db218502c1bd9ed2e6dd
Ransford Genesis <83247407+RansfordGenesis@users.noreply.github.com>
2023-06-01 23:59:08 +0000

    Update 169. Majority Element.py

--- LeetCode/169. Majority Element.py
+++ LeetCode/169. Majority Element.py
@@ -1,5 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        for num in nums:
-            if nums.count(num) > len(nums)/2:
-                return num
+        nums.sort()
+        return nums[len(nums)//2]
