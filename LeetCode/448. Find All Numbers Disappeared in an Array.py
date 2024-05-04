1cbeb6e1f5820107724433fb00b21fe98ac9652e
Ransford Genesis <83247407+RansfordGenesis@users.noreply.github.com>
2024-05-04 03:08:40 +0000

    Create 448. Find All Numbers Disappeared in an Array.py

---
+++ LeetCode/448. Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        new = set(nums)
+        
+        result = []
+        for i in range(1, len(nums)+1):
+            if i not in new:
+                result.append(i)
+        return result
+        
