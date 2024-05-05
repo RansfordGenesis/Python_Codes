ecd0afd6ce11cec6b24688734d88bff7a40e057c
Ransford Genesis <83247407+RansfordGenesis@users.noreply.github.com>
2024-05-05 01:15:33 +0000

    Create 832. Flipping an Image.py

---
+++ LeetCode/832. Flipping an Image.py
@@ -0,0 +1,6 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for i in range(len(image)):
+            image[i].reverse()
+            image[i] = [1 - each for each in image[i]]
+        return image
